4c8664b488bfafd7974df372441f33180bdf1e88
Timothe-picot <timothe.picot29@gmail.com>
2025-01-15 09:38:49 +0100

    Dernieres Modifs

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,8 @@ function Intro() {
 		<div className="intro">
 			<p>Bonjour, mon nom est</p>
 			<h1>Diane Monéger.</h1>
-			<h2>Je suis étudiante en BUT informatique.</h2>
+			<h2>Je suis alternante dans l'entreprise kwh50 et responsable du pôle BDD</h2>
+			<h2>Je suis aussi étudiante en BUT informatique.</h2>
 			<p>Voici mon portfolio.</p>
 		</div>
 		)
--- src/datas/ProjectList.js
+++ src/datas/ProjectList.js
@@ -1,103 +1,69 @@
-import github from '../assets/GitHub.png'
-import web from '../assets/Web.jpg'
-import unity from '../assets/Unity.jpg'
-import brez from '../assets/Brez.svg'
-import diarbenn from '../assets/Diarbenn.png'
-import miguel from '../assets/Miguel.png'
-import p4 from '../assets/Puissance 4.png'
-import quoiMaGeule from '../assets/moi.jpg'
-import mint from '../assets/mint.jpg'
-import cavale from '../assets/Vaush2.png'
-import Semaphore from '../assets/Proj-semaphore.jpg'
+import decovideur from '../assets/decovideur.png'
+import p4 from '../assets/p4.png'
+import logo_rectorat from '../assets/logo_rectorat.png'
+import logoloapique from '../assets/logo_loapique.png'
+import Logo_LivreaPiques from '../assets/Logo_LivreaPiques.png'
+import kwh_logo from '../assets/kwh_logo.png'
 
 export const projectList = [
 	{
 		id: '1',
-		cover: miguel,
-		name: "Bouh",
-		date: "Juin 2023",
-		desc: "Voici mon tout premier jeu vidéo. Bouh est un jeu d'horreur où vous incarnez un étudiant du crous de Lannion en pleine insomnie qui va rencontrer des phénomènes étranges en essayant de trouver un truc a manger. Étant mon tout premier jeu, je ne vous garantis pas une expérience à couper le souffle, mais j'espère pouvoir vous faire Peur/Rire. Le jeu est réalisé avec Unity et toutes les musiques sont libres de droits. Amusez-vous bien.",
-		learnt: "Implémenter des conceptions simples, Elaborer des conceptions simples, Faire des essais et évaluer les résultats en regard des spécifiactions et développer des interfaces utilisateurs",
-		languages: "C#, Unity",
-		link: "https://play.unity.com/u/Tripotausaure",
-		linkimg: unity,
-		category: 'Personnel'
+		cover: p4,
+		name: "puissance 4",
+		date: "Décembre 2022",
+		desc: "Il s'agit d'un Puisance 4 pour un entreprise fictive, les Têtes à Clic. Il permet à deux enfants d'en disputer une partie.",
+		learnt: "Compétence 1, Réaliser un développement d'application, Implémenter des conceptions simples, Faire des essais et évaluer leurs résultats en regard des spécifications, Compétence 2,Analyser le problème avec méthode, Comparer des algorithmes pour des problèmes classiques",
+		languages: "C",
+		category: 'IUT'
 	},
 	{
 		id: '2',
-		cover: p4,
-		name: "Puissance 4",
-		date: "Décembre 2022",
-		desc: "Lors de la SAE 1.01 et 1.02 de ma première année de BUT Informatique, j'ai été amené à développer un puissance 4 en C qui fonctionnait dans le terminal. Le joueur pouvait jouer au choix contre un autre joueur ou un algorithme réalisé.",
-		learnt: "Implémenter des conceptions simples, Faire des essais et évaluer leurs résultats en regard des spécifications, Analyser le problème avec méthode, Comparer des algorithmes pour des problèmes classiques",
-		languages: "C",
-		link: "https://github.com/Tripotausaure/Progs_test/tree/main/Puissance%204%20Solo",
-		linkimg: github,
-		category: 'IUT'
+		cover: decovideur,
+		name: "Décovideur",
+		date: "Mars 2022",
+		desc: "C'est un démineur version 'Covid' réalisé en groupe de 3 dans le cadre du projet final de terminale en NSI.",
+		learnt: "Compétence 1, Réaliser un développement d'application, Implémenter des conceptions, Elaborer des conceptions simples, Développer des interfaces utilisateur",
+		languages: "JavaScript, HTML, CSS",
+		category: 'Lycée'
 	},
 	{
 		id: '3',
-		cover: quoiMaGeule,
-		name: "Portfolio",
-		date: "Janvier 2024",
-		desc: "Suite à l'apprentissage de React, j'ai décidé de faire une refonte complète de mon portfolio. Cette nouvelle version le rend un peu plus présentable et me permet d'ajouter de nouveaux projets sans toucher au code",
-		learnt: "Appliquer des principes d’accessibilité et d’ergonomie, Adopter de bonnes pratiques de conception et de programmation, Vérifier et valider la qualité de l’application par les tests",
-		languages: "HTML, CSS, JS, React",
-		category: 'Personnel',
+		cover: logo_rectorat,
+		name: "Entrepôt de données agents",
+		date: "Avril 2024",
+		desc: "Dans le cadre de mon stage de deuxième année en parcours data/IA, j'ai conceptualisé et mis en place un entgrepôt de données répertiriant tous les agents de l'académie ainsi que leurs affectations à ce jour. J'ai puisé les informations de 11 sources defférentes et ai fait un ETL.",
+		learnt: "Compétence 4,  gérer des données de l'information , Mettre à jour et interroger une base de données relationnelle, Visualiser des données, Concevoir une base de données relationnelle à partir d'un cahier des charges",
+		languages: "PostGresSQL, Java",
+		category: 'Professionnel',
 	},
 	{
 		id: '4',
-		cover: mint,
-		name: "La Maison Jungle",
-		date: "Janvier 2024",
-		desc: "Site Web réalisé en suivant la formation react de OpenClassrooms. Ce site est un site de vente de plantes en ligne. Il permet de voir les plantes disponibles et de les ajouter au panier.",
-		learnt: "Suivre une formation en ligne, Utiliser React pour créer des applications web",
-		languages: "HTML, CSS, JS, React",
-		link: "https://timothe-picot.github.io/La-Maison-Jungle/",
-		linkimg: web,
-		category: 'Personnel'
-	},
-	{
-		id: '5',
-		cover: cavale,
-		name: "Le parcours du Cavalier",
-		date: "Avril 2023",
-		desc: "Durant la SAE 2.02 du BUT Informatique, nous avions pour projet de réaliser un programme qui permettait de trouver un chemin hamiltonien, pour un cavalier sur un échiquier de taille n*n. Cela consistait à le faire passer par toutes les cases sans repasser deux fois sur la même.",
-		learnt: "Analyser le problème avec méthode",
-		languages: "Python",
-		link: "https://github.com/Tripotausaure/Progs_test/tree/main/Probl%C3%A8me%20du%20cavalier",
-		linkimg: github,
+		cover: logoloapique,
+		name: "LocAPique",
+		date: "Année 2023-2024",
+		desc: "Pour simuler une expérience en entreprise, j'ai réalisé avec un groupe de 6 autres personne un site web sur le modéle de AirBnB.",
+		learnt: "Compétence 1, Implémenter des conceptions, faire des essais et évaluer les résultats en regard des spécifications, développer des interfaces utilisateur, Compétence 4, Gérer des données de l'information, concevoir une base de données relationnelle à partir d'un cahier des charges",
+		languages: "HTML, CSS, JS, PostGresSQL, PHP",
 		category: 'IUT'
 	},
 	{
+		id: '5',
+		cover: kwh_logo,
+		name: "CRUD BDD",
+		date: "Septembre 2025 - ce jour",
+		desc: "En tant qu'alternante en Base de Données, je m'occupe de faire une interface afin que les gens non-techniques puisse interagire avec.",
+		learnt: "Compétence 1, Elaborer des conceptions simples, Compétence 2, optimiser des applications informatiques, Analyser un problème avec méthodes, Compétence 4,  gérer des données de l'information , Mettre à jour et interroger une base de données relationnelle, Visualiser des données, Concevoir une base de données relationnelle à partir d'un cahier des charges, Compétence 6, travailler dans une équipe informatique, acquérir les compétences interpersonnelles pour travailler en équipe",
+		languages: "HTML, CSS, PHP, PostGresSQL",
+		category: 'Professionnel'
+	},
+	{
 		id: '6',
-		cover: Semaphore,
-		name: "Mini-Projet Semaphore",
-		date: "Fevrier 2024",
-		desc: "Dans le cadre d'un mini-projet pour une entreprise, j'ai développé une application permettant de visualiser une liste de sémaphores sur une carte interactive. L'objectif était de déterminer si un sémaphore avait été récemment visité ou non, offrant ainsi une vue claire de leur statut de visite.",
-		learnt: "Appliquer des principes d’accessibilité et d’ergonomie, Adopter de bonnes pratiques de conception et de programmation, Vérifier et valider la qualité de l’application par les tests",
-		languages: "HTML CSS JS W2UI Leaflet",
-		link: "https://timothe-picot.github.io/Mini-Projet-S-maphores-/",
-		linkimg: web,
-		category: 'Personnel'
-	},
-	{
-		id:"7",
-		cover:brez,
-		name:"Alaiz-Breiz",
-		date:"Septembre 2023 - Avril 2024",
-		desc:"Dans le cadre d'un projet de groupe à l'IUT, nous avons réalisé une application de réservation de logements style Air BNB. dans ce projet j'ai surtout travaillé sur la partie Front ca étant en équipe de 7 nous avions réparti le travail par préférences",
-		learnt:"Partir des éxigences et aller jusqu'à une application complète, Optimiser une base de donnéés, appliquer une démarche de suivi de projet en fonction des besoins métiers des clients et des utilisateurs, Situer son rôle et ses missions au sein d'une équipe informatique",
-		languages:"HTML CSS JS PHP PostgreSQL",
-		category:"IUT"
-	},
-	{
-		id:"8",
-		cover:diarbenn,
-		name:"Diarbenn",
-		date:"2024",
-		desc:"Lors de mon alternance au sein du Laboratoire Numérique CECLANT pour la marine nationale, j'ai travaillésur le projet DIARBENN. Ce projet consistait à faire l'inventaire de tout les batiment de l'ile longue pour déterter les zones à risque afin de prévenir les incendies.",
-		learnt:"Choisir et implémenter les architectures adaptées, Faire évoluer une application existante, Intégrer des solutions dans un environnement de production",
-		languages:"TypeScript Angular W2UI NodeJS Sequelise",
-		category:"Pro"}
+		cover: Logo_LivreaPiques,
+		name: "LivreAPic",
+		date: "Septembre 2025 - ce jour",
+		desc: "Lors de mes études, j'ai développé une application de recommandation de livres avec une intelligence artificielle.",
+		learnt: "Compétence 1, Implémenter des conceptions, faire des essais et évaluer les résultats en regard des spécifications, développer des interfaces utilisateur, Compétence 4, Gérer des données de l'information, concevoir une base de données relationnelle à partir d'un cahier des charges",
+		languages: "HTML, CSS, PHP, PostGresSQL",
+		category: 'IUT'
+	}
 ]
